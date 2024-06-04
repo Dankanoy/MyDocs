fbb8a72d4b0223079a3f6b807a0b20734f3fe3ce
heider <heiderazaresceo@gmail.com>
2024-06-04 08:30:44 +0800

    V5 beta

--- docs.js
+++ docs.js
@@ -2,3 +2,4 @@ const doc = console.log('hello im a doc! but I am from javascript');
 const doc3 = 1244;
 const doc4 = "hey!";
 const bug = "BugFix";
+const newFeatures = console.log("i add a new features");
