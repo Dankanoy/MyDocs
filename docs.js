12e09da966ee4ba6e97f98eed466de7ffcb902ce
heider <heiderazaresceo@gmail.com>
2024-06-04 09:06:38 +0800

    conflict  fixed

--- docs.js
+++ docs.js
@@ -1,6 +1,5 @@
 const doc = console.log('hello im a doc! but I am from javascript');
 const doc3 = 1244;
 const doc4 = "hey!";
-const bug = "BugFix";
 const newFeatures = console.log("i add a new features");
 console.log("please dont forget the git remote");
